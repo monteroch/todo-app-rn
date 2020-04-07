9d6e83805635f4da29246254ee8296f6103749c2
Christian Montero Martínez <christianmontero08@gmail.com>
2020-04-06 21:31:19 -0700

    Add remove icon and flexbox layout

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Alert, View, FlatList, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoItem';
+import AddTodo from './components/addTodo';
+import SandBox from './components/sandbox';
 
 export default function App() {
 
@@ -9,28 +11,46 @@ export default function App() {
     {text: 'buy coffee', key:'1'},
     {text: 'crate an app', key:'2'},
     {text: 'play on the switch', key:'3'}
-  ])
+  ]);
 
   const pressHandler = (key) => {
     setTodos((prevTodos) => {
       return prevTodos.filter( todo => todo.key != key) 
     });
+  };
+
+  const submitHandler = (text) => {
+    if(text.length > 3){
+      setTodos((prevTodos) => {
+        return [
+          {text: text, key: Math.random().toString()},
+          ...prevTodos
+        ]
+      });
+    }else{
+      Alert.alert('Oops!', 'Todos must be over 3 chars long', [
+        {text: 'Understood', onPress: () => console.log('alert closed')}
+      ]);
+    }
   }
 
   return (
-    <View style={styles.container}>
-      <Header/>
-      <View style={styles.content}>
-        {/* to form */}
-        <View style={styles.list}></View>
-          <FlatList
-            data={todos}
-            renderItem={({item}) => (
-              <TodoItem item={item} pressHandler={pressHandler}/>
-            )}
-          />
+    <TouchableWithoutFeedback onPress={() => {Keyboard.dismiss()}}>
+      <View style={styles.container}>
+        <Header/>
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler}/>
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({item}) => (
+                <TodoItem item={item} pressHandler={pressHandler}/>
+              )}
+            />
+          </View>  
+        </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -41,8 +61,10 @@ const styles = StyleSheet.create({
   },
   content: {
     padding: 40,
+    flex: 1
   },
   list: {
-    marginTop: 20
+    marginTop: 20,
+    flex: 1
   }
 });
--- components/todoItem.js
+++ components/todoItem.js
@@ -1,22 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function TodoItem({ item, pressHandler }){
     return(
         <TouchableOpacity onPress={() => pressHandler(item.key)}>
-            <Text style={styles.item} >{item.text}</Text>
+            <View style={styles.item}>
+                <Text style={styles.itemText}>{item.text}</Text>
+                <MaterialIcons style={styles.itemIcon} name='delete' size={20} color='#333'/>
+            </View>
         </TouchableOpacity>
     )
 }
 
 const styles = StyleSheet.create({
     item: {
-        padding: 16,
+        flex: 1,
+        padding: 12,
         marginTop:16,
         borderColor: '#bbb',
         borderWidth: 1,
         borderStyle: 'dashed',
-        borderRadius: 10
-
-    }
+        borderRadius: 10,
+        flexDirection: 'row',
+        justifyContent: "space-between"
+    },
 })
